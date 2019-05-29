b51ef4c25a28339bd59d01f8dd2152eefeb17aa7
Chandler DeLoach <deloachcd@g.cofc.edu>
2019-05-29 01:06:58 -0400

    implemented support for negative numbers in lists

--- Day00/solution.py
+++ Day00/solution.py
@@ -10,19 +10,20 @@ def linear_solution(lst, k, **kwargs):
     # This is a linear time solution - it uses a hashing function and a table to
     # determine if there are two elements in lst which sum to k in linear time
     def simple_hash(list_element):
-        # This 'simple' hash function supports lists of positive numbers
         return min([list_element, k-list_element])
 
-    # If two numbers 'hit' the same 'box' (hash to the same index in the table),
-    # they sum to k
-    hitboxes = [False for i in range((k//2)+1)]
+    # In a language without "dynamic array"-like lists, cells can be implemented
+    # as linked lists
+    cells = [[] for i in range((k//2)+1)]
     for _, element in enumerate(lst):
-        if element >= 0 and element <= k:
-            elmnt_hash = simple_hash(element)
-            if 'DEBUG' in kwargs:
-                print("{} -> {}".format(element, elmnt_hash))
-            if hitboxes[elmnt_hash]:
+        # TODO explain how this works in comments
+        negative = element < 0
+        distance = (-1 if negative else 1) * element // (-k if negative else k)
+        reduced_element = abs(element) % k
+        hash_cell = cells[simple_hash(reduced_element)]
+        for recorded_distance in hash_cell:
+            if distance + recorded_distance == 0:
                 return True
-            else:
-                hitboxes[elmnt_hash] = True
+        hash_cell.append(distance)
+
     return False
--- Day00/test_solution.py
+++ Day00/test_solution.py
@@ -1,9 +1,14 @@
 from solution import linear_solution
 
 
-def test_linear_solution_positive_integers():
+def test_linear_solution_positives_only():
     assert linear_solution([1, 2, 3], 1) is False
     assert linear_solution([1, 2, 3], 5) is True
     assert linear_solution([10, 15, 3, 7], 17) is True
     assert linear_solution([10, 15, 3, 7], 10) is True
     assert linear_solution([10, 15, 3, 7], 7) is False
+
+
+def test_linear_solution_with_negatives():
+    # TODO write more tests here
+    assert linear_solution([-18, 15, 3, 35], 17) is True
